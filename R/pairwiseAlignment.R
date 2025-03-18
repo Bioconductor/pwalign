7bd00dced8e78e025f64b29f1612bfe2f97ded5c
Hervé Pagès <hpages.on.github@gmail.com>
2025-03-18 08:00:05 -0700

    Replace
      array(unlist(m, m), c(dim(m), 2))
    
    with
    
      array(c(m, m), c(dim(m), 2))

--- R/pairwiseAlignment.R
+++ R/pairwiseAlignment.R
@@ -89,7 +89,7 @@ function(pattern,
            ncol = length(availableLetters),
            dimnames = list(availableLetters, availableLetters))
   substitutionArray <-
-    array(unlist(substitutionMatrix, substitutionMatrix), dim = c(dim(substitutionMatrix), 2),
+    array(c(substitutionMatrix, substitutionMatrix), dim = c(dim(substitutionMatrix), 2),
           dimnames = list(availableLetters, availableLetters, c("0", "1")))
   substitutionLookupTable <-
     buildLookupTable(alphabetToCodes[availableLetters],
